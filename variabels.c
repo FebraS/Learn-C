4aa893157206b8c2d249b80be6aa0bcbec34fdd2
Febra <0xfebra@gmail.com>
2025-08-02 09:41:20 +0800

    Create variabels.c

---
+++ variabels.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    
+    // int adalah bilangan bulat
+    // float dan double adalah bilangan desimal
+    // char adalah karakter 
+    // char name[] adalah string atau untaian karakter
+    // bool memiliki dua nilai yaitu benar atau salah
+    
+    int age = 25; // 4 bytes
+    float gpa = 3.5; // 4 bytes
+    double pi = 3.14159265358979323846; // 8 bytes
+    char grade = 'A'; // 1 bytes
+    char name[] = "Febra S"; // 8 bytes (8 char + null)
+    bool isOnline = true; // 1 bytes (membutuhkan library stdbool)
+
+    printf("%d\n",age);
+    printf("%.2f\n",gpa);
+    printf("%.15lf\n",pi);
+    printf("%c\n",grade);
+    printf("%s\n",name);
+    printf("%d\n",isOnline);
+    
+    return 0;
+}
