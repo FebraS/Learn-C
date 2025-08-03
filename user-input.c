e6140ce9785f1484d25c6d1be5665b9e62556974
Febra <0xfebra@gmail.com>
2025-08-03 10:17:15 +0800

    Update user-input.c

--- user-input.c
+++ user-input.c
@@ -4,8 +4,9 @@ int main(){
     
     int age;
     float gpa;
-    char grade;
+    char grade = '\n';
     char name[30] = "";
+    
     
     printf("Enter your age : ");
     scanf("%d",&age);
@@ -13,10 +14,12 @@ int main(){
     printf("Enter your gpa : ");
     scanf("%f",&gpa); // Input ini akan meninggalkan karakter newline
     
+    
     // Kita wajib membersihkan buffer sebelum memanggil scanf lagi, 
     // terutama jika Anda menggunakan %c. Jika tidak, 
     // karakter newline (\n) dari input sebelumnya akan 
     // tertinggal di buffer dan langsung terbaca oleh scanf berikutnya. 
+    
     
     // Cara termudah untuk membersihkan karakter newline bisa
     // dengan menambahkan spasi sebelum specifier
@@ -25,7 +28,9 @@ int main(){
     scanf(" %c",&grade); // Menambahkan spasi sebelum spesifier char (%c)
     
     printf("Enter your name : ");
-    scanf("%s",&name);
+    //scanf("%s",&name);
+    // Kita juga bisa menggunakan fungsi fgets
+    fgets(name,30,stdin);
     
     printf("Age   :%d\n",age);
     printf("GPA   :%f\n",gpa);
